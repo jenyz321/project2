75a0e3215a82011b37b2dc8518406dc3bee79c51
messejessie <messejessie77@gmail.com>
2019-03-28 15:21:22 -0400

    working on tie to questions still

--- server.js
+++ server.js
@@ -41,11 +41,11 @@ if (process.env.MONGODB_URI) {
   mongoose.connect(databaseUri);
 }
 var db = mongoose.connection;
-db.on("error", function(err) {
+db.on("error", function (err) {
   console.log("Mongoose Error:" + err);
 });
 
-db.once("open", function() {
+db.once("open", function () {
   console.log("Mongoos connection successful.");
 });
 
@@ -62,34 +62,34 @@ db.once("open", function() {
 // });
 
 //Routes
-app.get("/users", function(req, res) {
+app.get("/users", function (req, res) {
   Members.find({})
-    .then(function(dbMember) {
+    .then(function (dbMember) {
       console.log("dbMember", dbMember);
       // If any Books are found, send them to the client
       res.json(dbMember);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurs, send it back to the client
       res.json(err);
     });
 });
 
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
   res.render("registration");
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.get("/questions", function(req, res) {
+app.get("/questions", function (req, res) {
   res.render("profile");
 });
 
-app.post("/login", function(req, res) {
+app.post("/login", function (req, res) {
   Members.findOne({ email: req.body.loginEmail })
-    .then(function(dbMember) {
+    .then(function (dbMember) {
       console.log(req.body.loginEmail);
 
       console.log("dbMember", dbMember);
@@ -103,45 +103,46 @@ app.post("/login", function(req, res) {
         res.render("profile", app.locals.user);
       }
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurs, send it back to the client
       res.json(err);
     });
 });
 
-app.post("/registerSubmit", function(req, res) {
+app.post("/registerSubmit", function (req, res) {
   // Create a new member in the database
   Members.create(req.body)
-    .then(function(dbMember) {
+    .then(function (dbMember) {
       //getMatches(req.body);
       console.log("dbMember", dbMember);
       // If the member was updated successfully, send it back to the client
       res.render("index", dbMember);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurs, send it back to the client
       console.log("There was an error signing up!");
       console.log(res.json(err));
     });
 });
 
-app.post("/submitQuestions/:id", function(req, res) {
-  Members.findByIdAndUpdate(answers)
-    .then(function(answers) {
-      console.log("answers", answers);
-      res.render("profile", answers);
-    })
-    .catch(function(err) {
-      // If an error occurs, send it back to the client
-      console.log("There was an error saving your answers!");
-      console.log(res.json(err));
-    });
+app.post("/submitQuestions/:id", function (req, res) {
+  // Members.updateOne(answers)
+  //   // Members.findByIdAndUpdate(answers)
+  //   .then(function (answers) {
+  //     console.log("answers", answers);
+  //     res.render("profile", answernods);
+  //   })
+  //   .catch(function (err) {
+  //     // If an error occurs, send it back to the client
+  //     console.log("There was an error saving your answers!");
+  //     console.log(res.json(err));
+  //   });
 
-  // a = Object.values(req.body);
-  // id = req.params.id;
-  // console.log(a);
-  // a.forEach(function(e) {
-  //   questionsUpdate(e, id);
+  a = Object.values(req.body);
+  id = req.params.id;
+  console.log(a);
+  a.forEach(function(e) {
+    questionsUpdate(e, id);
 });
 
 //THIS IS JOE'S PROBLEM POINT
@@ -151,22 +152,22 @@ app.post("/submitQuestions/:id", function(req, res) {
 // res.render("profile", app.locals.user);
 // });
 
-app.post("/submitLanguages/:id", function(req, res) {
+app.post("/submitLanguages/:id", function (req, res) {
   a = Object.values(req.body);
   id = req.params.id;
   console.log(a);
-  a.forEach(function(e) {
+  a.forEach(function (e) {
     languagesUpdate(e, id);
   });
 });
 
-app.delete("/delete/:id", function(req, res) {
+app.delete("/delete/:id", function (req, res) {
   // Remove a note using the objectID
   Members.findOneAndRemove(
     {
       _id: req.params.id
     },
-    function(error, removed) {
+    function (error, removed) {
       // Log any errors from mongojs
       if (error) {
         console.log(error);
@@ -181,7 +182,7 @@ app.delete("/delete/:id", function(req, res) {
   );
 });
 //module.exports = app;
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
 
@@ -191,7 +192,7 @@ function questionsUpdate(a, id) {
     {
       $push: { answers: a }
     }
-  ).then(function(dbMember) {
+  ).then(function (dbMember) {
     console.log(dbMember);
   });
 }
@@ -202,7 +203,7 @@ function languagesUpdate(a, id) {
     {
       $push: { languages: a }
     }
-  ).then(function(dbMember) {
+  ).then(function (dbMember) {
     console.log(dbMember);
   });
 }
